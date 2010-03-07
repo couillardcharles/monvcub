0dc01ef392c9efd8b75d5fb3b626f4549f7163c9
charles <charles@192.168.0.11>
2010-03-08 00:06:07 +0100

    mise en page

--- src/com/appspot/monvcub/RessourceAccueil.java
+++ src/com/appspot/monvcub/RessourceAccueil.java
@@ -13,13 +13,13 @@ import com.google.appengine.repackaged.com.google.common.collect.Lists;
 public class RessourceAccueil extends ServerResource {
 	@Get
 	public StringRepresentation represente() throws ParserException, IOException {
-		StringBuilder builder = new StringBuilder("Nom\t\tVélos Dispos\t\tPlaces Dispos\n");
+		StringBuilder builder = new StringBuilder("Nom Vélos Dispos Places Dispos\n");
 
 		for (Station station : new ServiceStations().getStations(getStationsPréférées())) {
 			if (station.estEnMaintenance()) {
 				builder.append(station.getNom() + " EN MAINTENANCE\n");
 			} else {
-				builder.append(String.format("%s\t\t%d\t\t%d\n", station.getNom(), station.getVélosDisponibles(), station
+				builder.append(String.format("%s %d %d\n", station.getNom(), station.getVélosDisponibles(), station
 						.getPlacesDisponibles()));
 			}
 		}
